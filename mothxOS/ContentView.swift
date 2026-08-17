77ae0eaac9c979f7161acf5ae69cff33080c8503
yangdongfeng <yangjianjun@oschina.net>
2026-08-17 14:10:01 +0800

    Initial Commit

---
+++ mothxOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mothxOS
+//
+//  Created by YangDongFeng on 2026/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mothxOS/mothxOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  mothxOSApp.swift
+//  mothxOS
+//
+//  Created by YangDongFeng on 2026/8/17.
+//
+
+import SwiftUI
+
+@main
+struct mothxOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
